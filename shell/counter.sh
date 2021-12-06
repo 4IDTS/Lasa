f85250768361f3c055c406b480921c41a5631f89
Thiccy <birhic023@gmail.com>
2021-12-06 06:14:16 +0300

    Create counter.sh

---
+++ shell/counter.sh
@@ -0,0 +1,15 @@
+num={NUMBER}
+case $num in
+100)
+echo "{100ECHO}" ;;
+200)
+echo "{200ECHO}" ;;
+300)
+echo "{300ECHO}" ;;
+400)
+echo "{400ECHO}" ;;
+500)
+echo "{500ECHO}" ;;
+*)
+echo "false" ;;
+esac
